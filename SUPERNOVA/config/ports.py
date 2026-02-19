e214cdc17d61d6b8d6085fa0b0f228ec898dcb50
yahia ahmad abdallah ahmad <yehyageronimo@gmail.com>
2026-02-20 00:55:20 +0300

    Update and rename ports.py.py to ports.py

---
+++ SUPERNOVA/config/ports.py
@@ -0,0 +1,38 @@
+"""
+Common Ports Configuration
+"""
+
+COMMON_PORTS = {
+
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+
+    80: "HTTP",
+    110: "POP3",
+    111: "RPC",
+    135: "MSRPC",
+    139: "NetBIOS",
+
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+
+    993: "IMAPS",
+    995: "POP3S",
+
+    1433: "MSSQL",
+    1521: "Oracle DB",
+    3306: "MySQL",
+
+    3389: "RDP",
+
+    5432: "PostgreSQL",
+
+    5900: "VNC",
+
+    8080: "HTTP Proxy",
+
+}
