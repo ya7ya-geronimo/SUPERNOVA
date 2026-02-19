b04ddf31fb27e528d3018774c4c8b429244bab35
yahia ahmad abdallah ahmad <yehyageronimo@gmail.com>
2026-02-20 00:57:50 +0300

    Create paths.py

---
+++ SUPERNOVA/config/paths.py
@@ -0,0 +1,11 @@
+"""
+Project Paths
+"""
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+REPORT_DIR = os.path.join(BASE_DIR, "reports")
